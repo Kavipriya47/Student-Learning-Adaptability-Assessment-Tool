6c8fd8481ebd5df4a123da15e5486602a725b7e8
Kavipriya47 <kavipriyaponnusamy47@gmail.com>
2026-02-14 09:18:26 +0530

    Added client and server folders properly

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './Components/Login';
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to the Student Learning Adaptability Assessment Tool!</p>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
