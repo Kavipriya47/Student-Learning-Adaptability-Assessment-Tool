af1b355e135ca6aa801fe81d481d74b38cb52117
Kavipriya47 <kavipriyaponnusamy47@gmail.com>
2026-04-07 11:36:19 +0530

    fix: replace nodemailer SMTP with Resend HTTP API to bypass Render port restrictions

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,27 +1,11 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 15000,
-});
-
-exports.transporter = transporter;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 exports.sendEmail = async ({ to, subject, html }) => {
-    return await transporter.sendMail({
-        from: `"SLAA Platform" <${process.env.EMAIL_USER}>`,
-        to,
+    return await resend.emails.send({
+        from: `SLAA Platform <onboarding@resend.dev>`,
+        to: [to],
         subject,
         html,
     });
